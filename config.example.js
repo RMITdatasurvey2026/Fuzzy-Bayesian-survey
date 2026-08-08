956fc646ac2ccef1bd25fdd1efc39fbc8218e266
RMITdatasurvey2026 <73330413+RMITdatasurvey2026@users.noreply.github.com>
2026-08-08 21:00:23 +0700

    Update appScriptUrl in config.example.js

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 window.SURVEY_CONFIG = {
   // Copy this file to config.js and fill in your deployed Google Apps Script Web App URL.
-  appScriptUrl: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec"
+  appScriptUrl: "https://script.google.com/macros/s/AKfycbyJGz9KjON4Di3LGxUWZ7M8ooSNwIxycL2YnZyEvnH5IhkH2ZofbR5OTR9QQPHZy7xg/exec"
 };
