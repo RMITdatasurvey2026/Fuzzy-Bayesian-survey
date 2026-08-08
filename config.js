289d16f5869afc82a3863d356d938c0f274ea925
RMITdatasurvey2026 <73330413+RMITdatasurvey2026@users.noreply.github.com>
2026-08-08 22:41:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.SURVEY_CONFIG = {
-  appScriptUrl: "https://script.google.com/macros/s/AKfycbyJGz9KjON4Di3LGxUWZ7M8ooSNwIxycL2YnZyEvnH5IhkH2ZofbR5OTR9QQPHZy7xg/exec"
+  appScriptUrl: "https://script.google.com/macros/s/AKfycbyJGz9KjON4Di3LGxUWZ7M8ooSNwIxycL2YnZyEvnH5IhkH2ZofbR5OTR9QQPHZy7xg/exec",
+  nextSurveyUrl: "https://rmitdatasurvey2026.github.io/Fuzzy-Bayesian-survey/"
 };
